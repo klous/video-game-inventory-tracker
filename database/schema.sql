f3b85fdbb0f5057338e5fed9db5c43a6c96a998c
Scott Kloustin <532080+klous@users.noreply.github.com>
2022-03-06 09:43:57 -0500

    fixed schema FK and DROP table order

--- database/schema.sql
+++ database/schema.sql
@@ -2,7 +2,6 @@ DROP TABLE IF EXISTS system_variant_users_own;
 DROP TABLE IF EXISTS games_users_own;
 DROP TABLE IF EXISTS system_variant;
 DROP TABLE IF EXISTS game;
-DROP TABLE IF EXISTS system_variant;
 DROP TABLE IF EXISTS manufacturer;
 DROP TABLE IF EXISTS game_system;
 DROP TABLE IF EXISTS users;
@@ -37,8 +36,8 @@ CREATE TABLE system_variant (
 	system_variant_name varchar(100) NOT NULL,
 	manufacturer_id int NOT NULL,
 	
-	CONSTRAINT pk_system_variant PRIMARY KEY(system_variant_id),
-	CONSTRAINT fk_system_variant FOREIGN KEY(manufacturer_id) REFERENCES manufacturer(manufacturer_id)
+	CONSTRAINT pk_system_variant PRIMARY KEY (system_variant_id),
+	CONSTRAINT fk_system_variant_id FOREIGN KEY (manufacturer_id) REFERENCES manufacturer(manufacturer_id)
 );
 
 CREATE TABLE game (
@@ -46,31 +45,31 @@ CREATE TABLE game (
 	game_system_id int NOT NULL,
 	game_name varchar(150) NOT NULL,
 	description varchar(300),
-	physical boolean NOT NULL
+	physical boolean NOT NULL,
 	
-	CONSTRAINT pk_game PRIMARY KEY(game_id),
-	CONSTRAINT fk_game_game_system FOREIGN KEY(game_system_id) REFERENCES game_system(game_system_id)
+	CONSTRAINT pk_game PRIMARY KEY (game_id),
+	CONSTRAINT fk_game_game_system_id FOREIGN KEY(game_system_id) REFERENCES game_system(game_system_id)
 
 	
 );
 
-CREATE TABLE games_users_own {
+CREATE TABLE games_users_own (
 	user_id int NOT NULL,
 	game_id int NOT NULL,
 	quantity int NOT NULL,
 
-	CONSTRAINT pk_games_users_own PRIMARY KEY(user_id, game_id),
+	CONSTRAINT pk_games_users_own PRIMARY KEY (user_id, game_id),
 	CONSTRAINT fk_games_users_own_user_id FOREIGN KEY(user_id) REFERENCES users(user_id),
 	CONSTRAINT fk_games_users_own_game_id FOREIGN KEY(game_id) REFERENCES game(game_id)
-};
-}
+);
+
 CREATE TABLE system_variant_users_own (
 	user_id int NOT NULL,
-	system_variant_id int NOT NULL
+	system_variant_id int NOT NULL,
 	quantity int NOT NULL,
 
-	CONSTRAINT pk_system_variant_own PRIMARY KEY(user_id, system_variant_id),
-	CONSTRAINT fk_system_variant_own FOREIGN KEY(user_id) REFERENCES users(user_id),
-	CONSTRAINT fk_system_variant_own FOREIGN KEY(system_variant_id) REFERENCES system_variant(system_variant_id)
+	CONSTRAINT pk_system_variant_own PRIMARY KEY (user_id, system_variant_id),
+	CONSTRAINT fk_system_variant_own_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
+	CONSTRAINT fk_system_variant_own_system_variant_id FOREIGN KEY (system_variant_id) REFERENCES system_variant(system_variant_id)
 );
 
